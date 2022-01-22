3b5539b36805a472a4d2f54ef1ee876879fe22ad
Sorah Fukumori <sorah@cookpad.com>
2022-01-22 21:40:13 +0900

    PublicKeyLookup: use Cow

--- src/key_lookup.rs
+++ src/key_lookup.rs
@@ -1,12 +1,21 @@
 pub trait PublicKeyLookup {
-    fn lookup(&self, key_name: &str) -> Option<&jwt_simple::algorithms::ES256PublicKey>;
+    fn lookup(
+        &self,
+        key_name: &str,
+    ) -> Option<std::borrow::Cow<jwt_simple::algorithms::ES256PublicKey>>;
 }
 
 pub type PublicKeyBucket =
     std::collections::HashMap<String, jwt_simple::algorithms::ES256PublicKey>;
 
 impl PublicKeyLookup for PublicKeyBucket {
-    fn lookup(&self, key_name: &str) -> Option<&jwt_simple::algorithms::ES256PublicKey> {
-        self.get(key_name)
+    fn lookup(
+        &self,
+        key_name: &str,
+    ) -> Option<std::borrow::Cow<jwt_simple::algorithms::ES256PublicKey>> {
+        match self.get(key_name) {
+            Some(k) => Some(std::borrow::Cow::Borrowed(k)),
+            None => None,
+        }
     }
 }
